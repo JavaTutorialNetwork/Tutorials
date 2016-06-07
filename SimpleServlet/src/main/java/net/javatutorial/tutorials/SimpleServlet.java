33e09b391040d26103bf88fa2b4f2a9c8035d507
javtutorialnet <javatutorialnet@gmail.com>
2016-06-07 17:06:20 +0300

    initial commit

---
+++ SimpleServlet/src/main/java/net/javatutorial/tutorials/SimpleServlet.java
@@ -0,0 +1,29 @@
+package net.javatutorial.tutorials;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -4751096228274971485L;
+
+	@Override
+	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Hello World!");
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet " + this.getServletName() + " has started");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("Servlet " + this.getServletName() + " has stopped");
+	}
+	
+}
